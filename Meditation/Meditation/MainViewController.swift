f43c6aea312abc752541831074f4b462200381c5
knasya <71565061+knasya@users.noreply.github.com>
2022-09-13 13:49:45 +0300

    Add files via upload

---
+++ Meditation/Meditation/MainViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MainViewController.swift
+//  Meditation
+//
+//  Created by user on 08.09.2022.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
